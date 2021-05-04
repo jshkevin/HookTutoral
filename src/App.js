6d2ae8ad12ffe5d9a073a89734682ea12acf3109
jshkevin <jshkevin0603@gmail.com>
2021-05-04 20:35:00 +0900

    app change

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
-import React from 'react';
+import React ,{useRef}from 'react';
 import Average from './Average';
 import Info from './Info';
 import CounterClass from './CounterClass';
 import CounterFunction from './CounterFunction';
+import ContextSample from './Context';
+import CounterRecucer from './CounterReducer';
+import Layout from './Layout';
+
+
+function TextInputWithFocusButton() {
+  const inputEl = useRef(null);
+  const onButtonClick = () => {
+    // `current` points to the mounted text input element
+    inputEl.current.focus();
+  };
+  return (
+    <>
+      <input ref={inputEl} type="text" />
+      <button onClick={onButtonClick}>Focus the input</button>
+    </>
+  );
+}
+
 
 const App = () => {
   return (
     <div>
+      <Layout/>
+      {/* <TextInputWithFocusButton/> */}
+      {/* <InputSample/> */}
+      {/* <ContextSample/>
       <CounterClass/>
       <CounterFunction/>
+      <CounterRecucer/>
       <Info/>
-      <Average/>
+      <Average/> */}
     </div>
   )
 }
+
+
 
 export default App;
